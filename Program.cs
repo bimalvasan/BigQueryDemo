e301900530df072426ac9e74d95262a4a4241fb3
Bimal Vasan <bimalvasan@gmail.com>
2020-12-27 14:00:20 -0600

    BQ c# code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Google.Cloud.BigQuery.V2;
+
+namespace BigQueryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start...");
+	    var client = BigQueryClient.Create("qwiklabs-gcp-04-75f340426a0e");
+            var table = client.GetTable("bigquery-public-data", "samples", "shakespeare");
+            var sql = $"SELECT corpus AS title, COUNT(word) AS unique_words FROM {table} GROUP BY title ORDER BY unique_words DESC LIMIT 10";
+
+            var results = client.ExecuteQuery(sql, parameters: null);
+
+            foreach (var row in results)
+            {
+                Console.WriteLine($"{row["title"]}: {row["unique_words"]}");
+            }
+        }
+    }
+}
